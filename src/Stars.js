d5b453ea580d6582439da62f7936c4632228d5d4
Vikkelas <bass89@mail.ru>
2022-04-14 12:33:36 +0300

    homework react 3-1

---
+++ src/Stars.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Star from "./Star";
+function Stars({count}) {
+    if(count<1 || count > 5){
+        count = null;
+    }
+    const id = uuidv4();
+    const arrElement = [];
+    for(let i = 0; i <count; i+=1){
+      arrElement.push(<Star id={id}/>);
+    }
+    return (
+        <ul className={'card-body-stars'}>
+            {arrElement}
+        </ul>
+    );
+}
+export default Stars;
