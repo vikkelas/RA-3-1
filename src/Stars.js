c9c4ca08086af47f8366e4d3ca026ef4e3cd8a8e
Vikkelas <bass89@mail.ru>
2022-04-14 12:38:18 +0300

    homework react 3-1 proptypes

--- src/Stars.js
+++ src/Stars.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import Star from "./Star";
 function Stars({count}) {
@@ -16,4 +17,9 @@ function Stars({count}) {
         </ul>
     );
 }
+
+Stars.prototype = {
+    count: PropTypes.number.isRequired,
+}
+
 export default Stars;
